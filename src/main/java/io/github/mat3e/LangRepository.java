b9a04b68d3a7b5c22252ebfba5a4cd1012edad62
Qw8415 <g.szul00@gmail.com>
2019-07-16 22:38:29 +0200

    Add findAll() in LangRepository

--- src/main/java/io/github/mat3e/LangRepository.java
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -5,10 +5,23 @@ import java.util.List;
 import java.util.Optional;
 
 public class LangRepository {
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Lang", Lang.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
     Optional<Lang> findById(Integer id) {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
+
         var result = session.get(Lang.class, id);
+
         transaction.commit();
         session.close();
         return Optional.ofNullable(result);
